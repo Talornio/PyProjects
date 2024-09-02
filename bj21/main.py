b50b9e58fcdb67528eb965176fdb2e2abed3b32b
talornio <mattiacerulli@gmail.com>
2024-09-02 17:38:39 +0200

    altre modifiche

--- bj21/main.py
+++ bj21/main.py
@@ -11,12 +11,14 @@ def draw():
     return random.choice(cards)
 
 def game_init():
+    global punti_giocatore
     for i in range(2):
         mano_banco.append(draw())
         mano_giocatore.append(draw())
     print(f'\nMano del Banco: [{mano_banco[0]}, x]\n')
     print(f'La tua mano: {mano_giocatore}')
-    print(f'I tuoi punti: {calcola_punti(mano_giocatore, punti_giocatore)}\n')
+    punti_giocatore = calcola_punti(mano_giocatore, punti_giocatore)
+    print(f'I tuoi punti: {punti_giocatore}\n')
 
 def calcola_punti(mano, punti):
     for card in mano:
@@ -30,18 +32,38 @@ def calcola_punti(mano, punti):
     return punti
 
 def turno_giocatore():
+    global punti_giocatore
     tf = True
-    if punti_giocatore == 21:
-        print('21! Hai vinto!')
-    while punti_giocatore < 21 and tf == True:
-        yn = input('Peschi o stai? p/s')
-        if yn == 'p':
-            mano_giocatore.append(draw())
-            print(f'La tua mano: {mano_giocatore}')
-            print(f'I tuoi punti: {calcola_punti(mano_giocatore, punti_giocatore)}\n')
-        elif yn == 's':
-            print('Turno del banco!')
+    while tf:
+        if punti_giocatore == 21:
+            print('21! Hai vinto!')
             tf = False
+        if punti_giocatore < 21:
+            yn = input('Peschi o stai? p/s\n')
+            if yn == 'p':
+                mano_giocatore.append(draw())
+                print(f'La tua mano: {mano_giocatore}')
+                punti_giocatore = calcola_punti(mano_giocatore, punti_giocatore)
+                print(f'I tuoi punti: {punti_giocatore}\n')
+            elif yn == 's':
+                print('Turno del banco!')
+                tf = False
+        else: 
+            print('Hai sballato!')
+            tf = False
+
+def turno_banco():
+    global punti_banco
+    print(f'Mano del Banco: {mano_banco}')
+    punti_banco = calcola_punti(mano_banco, punti_banco)
+    print(f'I punti del Banco: {punti_banco}\n')
+    print
+    while punti_banco < 17:
+        print('Il Banco pesca!')
+        mano_banco.append(draw())
+        punti_banco = calcola_punti(mano_banco, punti_banco)
+        print(f'Mano del Banco: {mano_banco}')
+        print(f'I punti del Banco: {punti_banco}\n')
 
 
 game_init()
