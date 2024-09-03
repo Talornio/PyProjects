3389f54d85781e2597d8ecff30f6ea05dbcee02e
talornio <mattiacerulli@gmail.com>
2024-09-03 15:30:37 +0200

    yes

--- bj21/main.py
+++ bj21/main.py
@@ -6,12 +6,17 @@ mano_banco = []
 punti_banco = 0
 mano_giocatore = []
 punti_giocatore = 0
+vittoria_giocatore = False
+vittoria_banco = False
+
+global_flag = True
 
 def draw():
     return random.choice(cards)
 
 def game_init():
     global punti_giocatore
+    global punti_banco
     for i in range(2):
         mano_banco.append(draw())
         mano_giocatore.append(draw())
@@ -36,7 +41,7 @@ def turno_giocatore():
     tf = True
     while tf:
         if punti_giocatore == 21:
-            print('21! Hai vinto!')
+            print('21!')
             tf = False
         if punti_giocatore < 21:
             yn = input('Peschi o stai? p/s\n')
@@ -48,9 +53,18 @@ def turno_giocatore():
             elif yn == 's':
                 print('Turno del banco!')
                 tf = False
-        else: 
-            print('Hai sballato!')
-            tf = False
+
+def blackjack(mano):
+    punti = 0
+    for card in mano:
+        if card in ('re', 'donna', 'jack'):
+            punti += 10
+        elif card == 'asso':
+            punti += 11
+    if punti == 21:
+        print('Black Jack!')
+        return True
+    return False
 
 def turno_banco():
     global punti_banco
@@ -64,11 +78,29 @@ def turno_banco():
         punti_banco = calcola_punti(mano_banco, punti_banco)
         print(f'Mano del Banco: {mano_banco}')
         print(f'I punti del Banco: {punti_banco}\n')
-    if punti_banco > 17:
-        print('Il Banco ha sballato! Hai vinto!')
 
-
-game_init()
-turno_giocatore()
+while True:
+    game_init()
+    turno_giocatore()
+    if punti_giocatore > 21:
+        print('Hai sballato!')
+        break
+    elif blackjack(mano_giocatore):
+        punti_giocatore = 22
+    turno_banco()
+    if punti_banco > 21:
+        print('Il banco ha sballato!')
+        break
+    elif blackjack(mano_banco):
+        punti_banco = 22
+    if punti_banco > punti_giocatore:
+        print('Ha vinto il banco!')
+        break
+    if punti_banco < punti_giocatore:
+        print('Hai vinto!')
+        break
+    if punti_banco == punti_giocatore:
+        print('Pareggio!')
+        break
 
 
